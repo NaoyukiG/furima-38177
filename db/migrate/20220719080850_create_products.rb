440def961bb075711591f1cec100b35e9de5e378
NaoyukiG <naoyukineo@gmail.com>
2022-07-19 17:15:06 +0900

    マイグレーションの実行

--- db/migrate/20220719080850_create_products.rb
+++ db/migrate/20220719080850_create_products.rb
@@ -2,6 +2,15 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
 
+      t.string :title,            null: false
+      t.text :content,            null: false
+      t.integer :category_id,     null: false
+      t.integer :status_id,       null: false
+      t.integer :charge_id,       null: false
+      t.integer :prefacture_id,   null: false
+      t.integer :shipping_day_id, null: false
+      t.integer :price,           null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
