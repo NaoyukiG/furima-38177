6e34cacbe9b84d6a1215c03a6be5046ca79b0e7b
NaoyukiG <naoyukineo@gmail.com>
2022-07-19 17:47:54 +0900

    category.rbへのバリデーション、アクティブハッシュの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,20 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :price
+  end
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :prefacture_id
+    validates :shipping_day_id
+  end
+
+  validates :user, presence: true, foreign_key: true
+
 end
