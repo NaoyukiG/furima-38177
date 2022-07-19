d63165266c653b8e7a4786406642a3908f5f497b
NaoyukiG <naoyukineo@gmail.com>
2022-07-19 18:00:35 +0900

    ストロングパラメーターの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,11 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @products = Product.new
   end
-  
+
+  private
+  def product_params
+    params.require(:Product).permit(:title, :content, :category_id, :status_id, :charge_id, :prefacture_id, :shipping_day_id, :price).merge(user_id: :current_user.id)
+  end
 end
