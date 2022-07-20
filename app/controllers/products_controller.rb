2b8c08b3c08ac290924911483c0eaf404d4f4e79
NaoyukiG <naoyukineo@gmail.com>
2022-07-20 15:43:25 +0900

    createアクションの実装とルーティングの作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,10 @@ class ProductsController < ApplicationController
     @products = Product.new
   end
 
+  def create
+
+  end
+
   private
   def product_params
     params.require(:Product).permit(:title, :content, :category_id, :status_id, :charge_id, :prefacture_id, :shipping_day_id, :price, :image).merge(user_id: :current_user.id)
