f661bae780b1d2bfdee03cb025d4dba8f7e10b60
NaoyukiG <naoyukineo@gmail.com>
2022-07-21 12:13:21 +0900

    first-commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+    @product = Product.all
   end
 
   def new
@@ -20,5 +21,5 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:title, :content, :category_id, :status_id, :charge_id, :prefacture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
-  
+
 end
