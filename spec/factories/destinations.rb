7b650b7ec6417bf2044e43a92cd8eb77326c6294
NaoyukiG <naoyukineo@gmail.com>
2022-07-27 18:08:18 +0900

    destinationモデルのテストコードを記述

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :destination do
-    
+    postal_code { 123-4567 }
+    prefacture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Faker::Address.state  }
+    house_number { Faker::Address.city }
+    building_name { Faker::Address.building_number }
+    telephone_number { 19012345678 }
+    association :purchase_management
   end
 end
--- spec/factories/purchase_managements.rb
+++ spec/factories/purchase_managements.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_management do
-    association :user
     association :product
+    user { product.user }
     token {"tok_abcdefghijk00000000000000000"}
   end
 end
