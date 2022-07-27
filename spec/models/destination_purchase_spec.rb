c810e1441522ffde1e175efbef2b78df999a679e
NaoyukiG <naoyukineo@gmail.com>
2022-07-27 19:47:24 +0900

    テストコードの洗い出し

--- spec/models/destination_purchase_spec.rb
+++ spec/models/destination_purchase_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe DestinationPurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  end
+
+  describe '商品購入' do
+
+    context '内容に問題がない場合' do
+      it '全ての値が正しく入力されていれば商品が購入できる' do
+      end
+
+      it 'building_nameが空でも商品が購入できる' do
+      end
+      
+    end
+
+    context '内容に問題がある場合' do
+      it 'tokenが空だと商品が購入できない' do
+      end
+
+      it 'postal_codeが空だと商品が購入できない' do
+      end
+
+      it 'postal_codeは半角のハイフンを含んだ正しい形式でないと商品が購入できない' do
+      end
+
+      it 'prefacture_idが選択されていないと商品が購入できない' do
+      end
+      
+      it 'cityが空だと商品が購入できない' do
+      end
+      
+      it 'house_numberが空だと商品が購入できない' do
+      end
+      
+      it 'telephone_numberが空だと商品が購入できない' do
+      end
+      
+      it 'telephone_numberが10桁以上11桁以内の半角数値でないと商品が購入できない' do
+      end
+      
+      it 'userが紐づいていないと商品が購入できない' do
+      end
+
+      it 'productが紐づいていないと商品が購入できない' do
+      end
+
+    end
+
+
+  end
 end
