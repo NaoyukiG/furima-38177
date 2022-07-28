f158fec779f9c69e6ac2ac3a1de5e9b3a432e62c
NaoyukiG <naoyukineo@gmail.com>
2022-07-28 22:03:20 +0900

    バリデーションをwith options内へ記述

--- app/models/destination_purchase.rb
+++ app/models/destination_purchase.rb
@@ -4,25 +4,23 @@ class DestinationPurchase
                 :user_id, :product_id, :token
 
   with_options presence: true do
+    validates :postal_code,
+    format: {
+      with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)',
+      allow_blank: true
+    }
     validates :prefacture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
+    validates :telephone_number,
+    format: {
+      with: /\A^0[0-9]{9,10}$\z/, message: 'is invalid. Input half-width numbers',
+      allow_blank: true
+    }
     validates :user_id
     validates :product_id
     validates :token
   end
-
-  validates :postal_code, presence: true,
-                          format: {
-                            with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)',
-                            allow_blank: true
-                          }
-
-  validates :telephone_number, presence: true,
-                               format: {
-                                 with: /\A^0[0-9]{9,10}$\z/, message: 'is invalid. Input half-width numbers',
-                                 allow_blank: true
-                               }
 
   def save
     purchase_management = PurchaseManagement.create(user_id: user_id, product_id: product_id)
