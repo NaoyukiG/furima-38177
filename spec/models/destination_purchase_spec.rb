60f383d117fd492331162b9d7b6609c04ed72a4a
NaoyukiG <naoyukineo@gmail.com>
2022-07-28 14:51:41 +0900

    テストコードの記述を実装

--- spec/models/destination_purchase_spec.rb
+++ spec/models/destination_purchase_spec.rb
@@ -26,34 +26,62 @@ RSpec.describe DestinationPurchase, type: :model do
     context '内容に問題がある場合' do
       it 'tokenが空だと商品が購入できない' do
         @destination_purchase.token = ''
-        
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Token can't be blank")
       end
 
       it 'postal_codeが空だと商品が購入できない' do
+        @destination_purchase.postal_code = ''
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Postal code can't be blank")
       end
 
       it 'postal_codeは半角のハイフンを含んだ正しい形式でないと商品が購入できない' do
+        @destination_purchase.postal_code = '1234567'
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
 
       it 'prefacture_idが選択されていないと商品が購入できない' do
+        @destination_purchase.prefacture_id = 1
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Prefacture can't be blank")
       end
       
       it 'cityが空だと商品が購入できない' do
+        @destination_purchase.city = ''
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("City can't be blank")
       end
       
       it 'house_numberが空だと商品が購入できない' do
+        @destination_purchase.house_number = ''
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("House number can't be blank")
       end
       
       it 'telephone_numberが空だと商品が購入できない' do
+        @destination_purchase.telephone_number = ''
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Telephone number can't be blank")
       end
       
-      it 'telephone_numberが10桁以上11桁以内の半角数値でないと商品が購入できない' do
+      it 'telephone_numberが全角数値では商品が購入できない' do
+        @destination_purchase.telephone_number = '０９０１２３４５６７８'
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Telephone number is invalid. Input half-width numbers")
       end
       
       it 'userが紐づいていないと商品が購入できない' do
+        @destination_purchase.user_id = nil
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("User can't be blank")
       end
 
       it 'productが紐づいていないと商品が購入できない' do
+        @destination_purchase.product_id = nil
+        @destination_purchase.valid?
+        expect(@destination_purchase.errors.full_messages).to include("Product can't be blank")
       end
 
     end
